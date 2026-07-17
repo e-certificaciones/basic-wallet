46621d47160c144cb2e543c790f2300264740f81
e-certificaciones <e.antonio.certificaciones@gmail.com>
2026-07-17 10:32:24 -0600

    feat: validate-form-services.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, session
 from app.auth import auth_bp # importar el objeto BluePrint
+from app.auth.services import validate_input, validate_password
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
@@ -9,7 +10,56 @@ def login():
 @auth_bp.route("/register", methods=["GET", "POST"])
 def register():
 
-    return render_template("/auth/register.html")
+    if request.method == "POST":
+
+        data = {
+            'name' : request.form.get('input-name').strip(),
+            'last_name' : request.form.get('input-last-name').strip(),
+            'email' : request.form.get('input-email').strip(),
+            'username' : request.form.get('input-username').strip(),
+            'password' : request.form.get('input-password').strip(),
+            'confirm_password' : request.form.get('input-confirm-password').strip(),
+            'birthday' : request.form.get('input-birthday'),
+            'checkbox' : request.form.get('checkbox')
+        }
+
+        error = {}
+
+        if not validate_input(data['name'], "name"):
+            error['name'] = "must provide valid name"
+
+        if not validate_input(data['last_name'], "name"):
+            error['last_name'] = "must provide valid last name"
+        
+        if not validate_input(data['email'], "email"):
+            error['email'] = "must provide valid email"
+
+        if not validate_input(data['username'], "username"):
+            error['username'] = "must provide valid username"
+        
+        if validate_password(data['password'], data['confirm_password']) == -1:
+            error['password'] = "must provide valid password with 6 caracters. Only letters, numbers, (.), (-), and (_) are allowed."
+            error['confirm_password'] = "must provide valid password with 6 caracters. Only letters, numbers, (.), (-), and (_) are allowed."
+        elif validate_password(data['password'], data['confirm_password']) == 0:
+            error['password'] = "must provide the same password in both input"
+            error['confirm_password'] = "must provide the same password in both input"
+
+        if data['birthday'] == "":
+            error['birthday'] = "must provide valid date"
+
+        if data['checkbox'] is None: # las comparaciones para valores None se deben hacer con is None o is not NOne
+            error['checkbox'] = "you must check the box to confirm your information is correct."
+
+        if error:
+            return render_template("/auth/register.html", data=data, error=error)
+        else:
+            return redirect(url_for('auth.login'))
+    
+    # pasamos un diccionario vacio de error en el get, la idea es poder usar jinja para buscar errores y poner sus valores 
+    # el los p descriptivos de cada para cada campo input, como jinja no da errores si un valor que buscar no existe, solo no muestra nada
+    # usamos esta tecnica para no usar if anidados y que las validaciones del js tambien funcionen, sin usar un if elif para mostrar las 
+    # etiquetas p, esto no funciona para data
+    return render_template("/auth/register.html", error={}) 
 
 @auth_bp.route("/logout")
 def logout():
--- app/auth/services.py
+++ app/auth/services.py
@@ -2,7 +2,37 @@
     este archivo contiene la logica, como validaciones
     y funcionalidad del modulo
 """
+import re # para acceder a funciones de regular expresions
 
-def register_user(username, password, confirmation):
+REG_EXP_NAME = r"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$"  # la validacion para nombre y lastname es la misma
+REG_EXP_EMAIL = r"^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,}$"
+REG_EXP_USERNAME = r"^[a-zA-Z0-9_áéíóúÁÉÍÓÚñÑ.-]+$"
+REG_EXP_PASSWORD = r"^[a-zA-Z0-9._-]{6,}$"
 
-    return "must provide username and password"
+# value es la cadena a validar, validation_type el tipo de validacion a aplicar 
+def validate_input(value:str, validation_type:str):
+
+    if validation_type == "name" or validation_type == "lastname": 
+        if re.fullmatch(REG_EXP_NAME,value):
+            return True
+    elif validation_type == "email":
+        if re.fullmatch(REG_EXP_EMAIL,value):
+            return True
+    elif validation_type == "username":
+        if re.fullmatch(REG_EXP_USERNAME,value):
+            return True
+    
+    return False
+
+# retona -1 cuando los valores en los campos no cumples con las condicionees de una password
+# retorna 0 cuando cambas cumplen pero no son contresenias iguales
+# retorna 1 cuando son constrasenias iguales y cumplen con los requisitos 
+def validate_password(password:str, confirm_password:str):
+    
+    if re.fullmatch(REG_EXP_PASSWORD,password) and re.fullmatch(REG_EXP_PASSWORD, confirm_password):
+        if password == confirm_password:
+            return 1
+        
+        return 0
+    else:
+        return -1
