f7a292018ab97bd3da5e5a632e1e9543685b96e6
e-certificaciones <e.antonio.certificaciones@gmail.com>
2026-07-21 13:18:48 -0600

    feat: basic-data-dashboard

--- app/acounts/models.py
+++ app/acounts/models.py
@@ -20,3 +20,61 @@ def index_information(user_id:str):
         data[key] = user_data[key]
     
     return data
+
+# funcion para calcular el dinero enviado y recibido 
+def calculate_send_recipient_money(user_id:str):
+
+    db = get_db()
+
+    cursor = db.execute(
+        "SELECT id, acount_number FROM acounts WHERE user_id = ?", (user_id, ), 
+    )
+
+    acount = cursor.fetchone()
+    acount_id = acount['id']
+    acount_number = acount['acount_number']
+
+    # al usar fetchall se van a guardar todas las filas que cumplan las condiciones de la query
+    # estas filas se pueden interpretar como objetos {"key":"value"}
+    rows = db.execute (
+        '''
+            SELECT
+                t.amount,
+                sender.acount_number AS sender_acount_number,
+                recipient.acount_number AS recipient_acount_number
+            FROM transactions AS t
+            JOIN acounts AS sender
+                ON t.sender_acount_id = sender.id
+            JOIN acounts AS recipient
+                ON t.recipient_acount_id = recipient.id
+            WHERE t.sender_acount_id = ? OR t.recipient_acount_id = ?
+            ORDER BY t.created_at DESC
+        ''',
+        (acount_id, acount_id, ),
+    ).fetchall()
+
+    # podemos crear una lista de diccionario para los filas de tipo row de rows
+    data = []
+
+    # cada row es una fila de rows, donde cada fila de rows se puede iterar como un objeto o diccionario con clave-valor
+    # {'created_at': date}
+
+    for row in rows:
+        data.append({
+            'amount' : row['amount'],
+            'sender_acount_number' : row['sender_acount_number'],
+            'recipient_acount_number' : row['recipient_acount_number']
+        })
+    
+    send_money = 0
+    recipient_money = 0
+
+    for d in data:
+
+        if d['sender_acount_number'] == acount_number:
+            send_money += d['amount']
+        
+        if d['recipient_acount_number'] == acount_number:
+            recipient_money += d['amount']
+    
+    return send_money, recipient_money
--- app/acounts/routes.py
+++ app/acounts/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, url_for, session
 from app.acounts import acounts_bp # importar el blueprint
-from app.acounts.models import index_information
+from app.acounts.models import index_information, calculate_send_recipient_money
 from app.helpers import login_required
 
 @acounts_bp.route("/")
@@ -11,6 +11,7 @@ def index():
     
     data = index_information(user_id)
 
-    return render_template("/acounts/index.html", data=data)
+    send_money, recipient_money = calculate_send_recipient_money(user_id)
 
+    return render_template("/acounts/index.html", data=data, send_money=send_money, recipient_money=recipient_money)
 
